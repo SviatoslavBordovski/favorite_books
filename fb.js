498cc5865204c62208cfb29bece8b9b5a8a2da00
Sviatoslav.Bordovski <[sviatoslav.bordovski@temy.co]>
2020-10-27 19:30:06 +0200

    just adding project to the github

---
+++ fb.js
@@ -0,0 +1,33 @@
+// TODO: define addFavoriteBook(..) function
+
+// TODO: define printFavoriteBooks() function
+
+function addFavoriteBook(bookName) { // adding all books with bookName to 
+    // the 'favoriteBooks' array that do not include the 'Great' word
+    if (!bookName.includes("Great")){
+        favoriteBooks.push(bookName);
+    }
+}
+
+ function printFavoriteBooks() { // prints array length 
+    //and every bookName of the favoriteBooks array
+    console.log('Favorite Books: ' + favoriteBooks.length)
+
+    for(let bookName of favoriteBooks) {
+        console.log(bookName);
+    }
+}
+
+var favoriteBooks = [];
+
+addFavoriteBook("A Song of Ice and Fire");
+addFavoriteBook("The Great Gatsby");
+addFavoriteBook("Crime & Punishment");
+addFavoriteBook("Great Expectations");
+addFavoriteBook("You Don't Know JS");
+
+//console.log(favoriteBooks);
+
+// TODO: print out favorite books
+
+printFavoriteBooks();
